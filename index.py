56a60ccdd76e474237a46e69a9a36ea569487071
DINH HOANG <nguyendinhhoang.241206@gmail.com>
2026-07-10 19:49:25 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,14 @@
+a = int(input("Nhập a: "))
+b = int(input("Nhập b: "))
+if a < b:
+    print("a < b")
+elif a == b:
+    print("a = b")
+else:
+    print("a < b")
+
+a = [1, 2, 3 , 4]
+sum = 0
+for i in range(1, 4):
+    sum += a[i]
+print(sum)
